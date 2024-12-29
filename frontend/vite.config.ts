23796ee298132b8b3c7137234aa6c536baf19011
kota <52403688+yashikota@users.noreply.github.com>
2024-12-29 07:20:27 +0000

    fix: runtime error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,63 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+        input: {
+          "index.js": "virtual:react-router/server-build",
+        },
+      }
+      : undefined,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
-});
+  ssr: {
+    target: "webworker",
+    noExternal: true,
+    resolve: {
+      conditions: ["workerd", "browser"],
+    },
+    optimizeDeps: {
+      include: [
+        "react",
+        "react/jsx-runtime",
+        "react/jsx-dev-runtime",
+        "react-dom",
+        "react-dom/server",
+        "react-router",
+      ],
+    },
+  },
+  plugins: [
+    cloudflareDevProxy({
+      getLoadContext({ context }) {
+        return { cloudflare: context.cloudflare };
+      },
+    }),
+    reactRouter(),
+    {
+      name: "react-router-cloudflare-workers",
+      config: () => ({
+        build: {
+          rollupOptions: isSsrBuild
+            ? {
+              output: {
+                entryFileNames: "[name]",
+              },
+            }
+            : undefined,
+        },
+      }),
+    },
+    tsconfigPaths(),
+  ],
+}));
