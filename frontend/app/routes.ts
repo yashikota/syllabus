895ede8616610bcaec956357df072d10d7e98e37
kota <52403688+yashikota@users.noreply.github.com>
2024-12-30 03:54:45 +0000

    fix: remove about route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,5 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route(":syllabusID", "routes/syllabus.tsx"),
-  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
