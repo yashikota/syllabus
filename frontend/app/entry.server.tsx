66c48d782a08c6ac95f1f26fdaf37d04d8db5f24
yashikota <52403688+yashikota@users.noreply.github.com>
2025-01-05 22:29:35 +0900

    feat: hono-react-router-adapter

--- frontend/app/entry.server.tsx
+++ frontend/app/entry.server.tsx
@@ -1,44 +1,37 @@
-import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
-import type { AppLoadContext, EntryContext } from "react-router";
-import { ServerRouter } from "react-router";
+import { isbot } from 'isbot'
+import { renderToReadableStream } from 'react-dom/server'
+import type { AppLoadContext, EntryContext } from 'react-router'
+import { ServerRouter } from 'react-router'
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  routerContext: EntryContext,
-  _loadContext: AppLoadContext,
+  reactRouterContext: EntryContext,
+  // This is ignored so we can keep it in the template for visibility.  Feel
+  // free to delete this parameter in your app if you're not using it!
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  loadContext: AppLoadContext
 ) {
-  let shellRendered = false;
-  let statusCode = responseStatusCode;
-  const userAgent = request.headers.get("user-agent");
-
   const body = await renderToReadableStream(
-    <ServerRouter context={routerContext} url={request.url} />,
+    <ServerRouter context={reactRouterContext} url={request.url} />,
     {
+      signal: request.signal,
       onError(error: unknown) {
-        statusCode = 500;
-        // Log streaming rendering errors from inside the shell.  Don't log
-        // errors encountered during initial shell rendering since they'll
-        // reject and get logged in handleDocumentRequest.
-        if (shellRendered) {
-          console.error(error);
-        }
+        // Log streaming rendering errors from inside the shell
+        console.error(error)
+        responseStatusCode = 500
       },
-    },
-  );
-  shellRendered = true;
+    }
+  )
 
-  // Ensure requests from bots and SPA Mode renders wait for all content to load before responding
-  // https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
-  if ((userAgent && isbot(userAgent)) || routerContext.isSpaMode) {
-    await body.allReady;
+  if (isbot(request.headers.get('user-agent') || '')) {
+    await body.allReady
   }
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html')
   return new Response(body, {
     headers: responseHeaders,
-    status: statusCode,
-  });
+    status: responseStatusCode,
+  })
 }
