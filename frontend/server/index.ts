8fc6fad526d8dcf93e3e790f6fba8d8a626de579
yashikota <52403688+yashikota@users.noreply.github.com>
2025-04-09 00:16:10 +0900

    Revert "feat: add timezone"
    This reverts commit 620b19e51817c2685b0134fb7e0fa41fc6fb7dd4.

--- frontend/server/index.ts
+++ frontend/server/index.ts
@@ -85,7 +85,6 @@ function generateICS(syllabus: any[], year: string): Promise<string> {
           status: "CONFIRMED",
           transp: "OPAQUE",
           uid: Math.random().toString(36).substring(6),
-          timezone: "Asia/Tokyo",
         };
       });
     });
